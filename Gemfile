0ee02569116ff11dd365054c4f23476d5f083ac2
Shinichi Nakajima <nakajimabb@gmail.com>
2018-06-02 16:32:02 +0900

    add rails_admin gem

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'mysql2'
 gem 'devise'
+gem 'rails_admin'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin20520720', as: 'rails_admin'
   devise_for :users, module: :users
 end
