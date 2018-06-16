4d41f1fc2022c975c225f8fcefdef941d1896bf7
Shinichi Nakajima <nakajimabb@gmail.com>
2018-06-16 18:37:11 +0900

    modified some configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.gmail.com',
+    port:      	           587,
+    domain:                'example.com',
+    user_name:             'biblos.jp@gmail.com',
+    password:              'fZtYkW0DSJeo',
+    authentication:        'plain',
+    enable_starttls_auto:  true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
