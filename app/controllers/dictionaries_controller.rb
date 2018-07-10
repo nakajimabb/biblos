ca0acccc7a582e2d7fdf4bdc100c6bbaa1e1ce44
Shinichi Nakajima <nakajimabb@gmail.com>
2018-07-10 23:49:22 +0900

    some revisions

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,52 +1,61 @@
 class DictionariesController < ApplicationController
   def get
     if params[:lemma].present?
-      m = params[:lemma].match(/([GH])([0-9]+)/) if params[:lemma].present?
-
+      @vocabularies = Vocabulary.accessible(current_user.id)
+      m = params[:lemma].match(/([GH])(\d+)/) if params[:lemma].present?
       if m.present?
         @lemma = m[1] + m[2].to_i.to_s
-        @vocabularies = Vocabulary.accessible(current_user.id).where(lemma: @lemma)
+        @vocabularies = @vocabularies.where(lemma: @lemma).order('dictionaries.rank asc')
         bible_ids = VocabCount.where(lemma: @lemma).group(:bible_id).pluck(:bible_id)
         @bibles = Bible.where(id: bible_ids)
+      else
+        @vocabularies = @vocabularies.where('spell like ?', '%' + params[:lemma] + '%')
+        @lemma = @vocabularies.first.try(:lemma)
+        if @lemma.present?
+          bible_ids = VocabCount.where(lemma: @lemma).group(:bible_id).pluck(:bible_id)
+          @bibles = Bible.where(id: bible_ids)
+        else
+          @bibles = Bible.none
+        end
+      end
 
-        respond_to do |format|
-          format.html do
-            if params[:module_code].present? and params[:book_code].present?
-              select_bible = Bible.find_by(code: params[:module_code])
-              @vocab_count = VocabCount.find_by(bible_id: select_bible.id, book_code: params[:book_code], lemma: @lemma)
-              @vocab_indices = @vocab_count.vocab_indices.page(params[:page]) if @vocab_count.present?
-              used_bibles = current_user.valid_used_bibles.map { |used_bible| used_bible.bible }
-              used_bibles.sort_by!{ |bible| bible.rank }
-              @bible_names = used_bibles.map { |bible| [bible.code, bible.name] }.to_h
+      respond_to do |format|
+        format.html do
+          if params[:module_code].present? and params[:book_code].present?
+            select_bible = Bible.find_by(code: params[:module_code])
+            @vocab_count = VocabCount.find_by(bible_id: select_bible.id, book_code: params[:book_code], lemma: @lemma)
+            @vocab_indices = @vocab_count.vocab_indices.page(params[:page]) if @vocab_count.present?
+            used_bibles = current_user.valid_used_bibles.map { |used_bible| used_bible.bible }
+            used_bibles.sort_by!{ |bible| bible.rank }
+            @bible_names = used_bibles.map { |bible| [bible.code, bible.name] }.to_h
 
-              @passages = {}
-              @vocab_indices.each do |vocab_index|
-                passages = {}
-                used_bibles.each do |bible|
-                  passages2 = bible.get_passages(params[:book_code], vocab_index.chapter, vocab_index.verse, vocab_index.verse)
-                  if passages2.present?
-                    passages[bible.lang] ||= {}
-                    passages[bible.lang][bible.code] = passages2
-                  end
+            @passages = {}
+            @vocab_indices.each do |vocab_index|
+              passages = {}
+              used_bibles.each do |bible|
+                passages2 = bible.get_passages(params[:book_code], vocab_index.chapter, vocab_index.verse, vocab_index.verse)
+                if passages2.present?
+                  passages[bible.lang] ||= {}
+                  passages[bible.lang][bible.code] = passages2
                 end
-                @passages[vocab_index.id] = passages
               end
+              @passages[vocab_index.id] = passages
             end
-            @bread_crumb = [['聖書メニュー', nil], ['聖書語彙', dictionaries_get_path(lemma: params[:lemma])]]
-            render :get
           end
-          format.json do
-            vocabulary = @vocabularies.first
-            meaning = vocabulary.meaning
-            meaning += "\n"
-            @bibles.each do |bible|
-              meaning += bible.code + '(' + VocabCount.where(bible_id: bible.id, lemma: @lemma).sum(:count).to_s + ') '
-            end
-            hash = {spell: vocabulary.spell, lemma: vocabulary.lemma, meaning: meaning,
-                    pronunciation: vocabulary.pronunciation, transliteration: vocabulary.transliteration,
-                    lang: vocabulary.dictionary.lang}
-            render json: hash.try(:to_json)
+          @bread_crumb = [['聖書メニュー', nil], ['聖書語彙', dictionaries_get_path(lemma: params[:lemma])]]
+          render :get
+        end
+        format.json do
+          vocabulary = @vocabularies.first
+          meaning = helpers.truncate(vocabulary.meaning, length: 500)
+          meaning += "\n"
+          @bibles.each do |bible|
+            meaning += bible.code + '(' + VocabCount.where(bible_id: bible.id, lemma: @lemma).sum(:count).to_s + ') '
           end
+          hash = {spell: vocabulary.spell, lemma: vocabulary.lemma, meaning: meaning,
+                  pronunciation: vocabulary.pronunciation, transliteration: vocabulary.transliteration,
+                  lang: vocabulary.dictionary.lang}
+          render json: hash.try(:to_json)
         end
       end
     end
